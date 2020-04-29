ebc58394a75a07611f5e5e921591d128b4371a74
Ben <btr1975@gmail.com>
2020-04-29 00:05:57 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
+import os
 from ipaddresstools.ipaddresstools import __version__
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 packages = [
     'ipaddresstools'
@@ -14,6 +19,8 @@ setup(
     version=__version__,
     python_requires='~=3.3',
     description='This is a library used to manipulate and verify ipv4 address\'s. ucast and mcast',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='ipv4 ip multicast unicast network engineer',
     url='https://ipaddresstools.readthedocs.io',
     author='Benjamin P. Trachtenberg',
