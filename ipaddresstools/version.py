3e5abb16f5fcb4c19812f793f55e6865d9278739
Ben <btr1975@gmail.com>
2021-01-29 09:58:23 -0600

    Moved some setup araoun

---
+++ ipaddresstools/version.py
@@ -0,0 +1,16 @@
+"""
+Holds the version information for the package
+"""
+__title__ = 'ipaddresstools'
+__description__ = 'This is a library used to manipulate and verify ipv4 address\'s. ucast and mcast'
+__author__ = 'Benjamin P. Trachtenberg'
+__copyright__ = "Copyright (c) 2020 - 2021, Benjamin P. Trachtenberg"
+__credits__ = None
+__license__ = 'The MIT License (MIT)'
+__status__ = 'prod'
+__version_info__ = (1, 2, 10)
+__version__ = '.'.join(map(str, __version_info__))
+__maintainer__ = 'Benjamin P. Trachtenberg'
+__email__ = 'e_ben_75-python@yahoo.com'
+__url__ = 'https://ipaddresstools.readthedocs.io'
+
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 import os
-from ipaddresstools.ipaddresstools import __version__
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(base_dir, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+about = {}
+with open(os.path.join(base_dir, 'nso_jsonrpc_requester', 'version.py'), 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
 
 packages = [
     'ipaddresstools'
@@ -15,22 +18,22 @@ tests_require = [
 ]
 
 setup(
-    name='ipaddresstools',
-    version=__version__,
-    python_requires='>=3.3',
-    description='This is a library used to manipulate and verify ipv4 address\'s. ucast and mcast',
+    name=about['__title__'],
+    version=about['__version__'],
+    python_requires='>=3.5',
+    description=about['__description__'],
     long_description=long_description,
-    long_description_content_type='text/restructuredtext',
+    long_description_content_type='text/markdown',
     keywords='ipv4 ip multicast unicast network engineer',
-    url='https://ipaddresstools.readthedocs.io',
+    url=about['__url__'],
     project_urls={
         'Documentation': 'https://ipaddresstools.readthedocs.io/en/latest/',
         'Source': 'https://github.com/btr1975/ipaddresstools',
         'Tracker': 'https://github.com/btr1975/ipaddresstools/issues',
     },
-    author='Benjamin P. Trachtenberg',
-    author_email='e_ben_75-python@yahoo.com',
-    license='MIT',
+    author=about['__author__'],
+    author_email=about['__email__'],
+    license=about['__license__'],
     packages=packages,
     include_package_data=True,
     test_suite='pytest',
